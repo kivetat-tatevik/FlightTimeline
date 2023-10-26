265a671cc0a521b036870bcd0c11fb357e6ff156
kivetat-tatevik <kivetat.85@yandex.ru>
2023-10-26 12:13:58 +0200

    Initial Commit

---
+++ FlightTimeline/FlightTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTimelineApp.swift
+//  FlightTimeline
+//
+//  Created by Tatevik Khunoyan on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlightTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
